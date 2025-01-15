aae8fbb2d0eb1e9911918e73bb68bcd3eeb05659
surajguvaju079 <surajguvaju.079@kathford.edu.np>
2025-01-15 12:50:09 +0545

    all pages and css done

--- src/js/copyright.js
+++ src/js/copyright.js
@@ -2,5 +2,24 @@ const date = new Date();
 currentDate = date.getFullYear();
 const footer = document.querySelector("footer");
 const newElement = document.createElement("p");
-newElement.innerHTML = `<h2>&copy; ${currentDate}  SquareLearn | All Rights Reserved</h2>`;
+newElement.innerHTML = `&copy; ${currentDate}  SquareLearn | All Rights Reserved`;
 footer.appendChild(newElement);
+
+function moreOption(data) {
+  const answer = document.querySelectorAll(".faq-answer");
+  const more = document.querySelectorAll(".more");
+  answer.forEach((element, index) => {
+    if (Number(data) === index + 1) {
+      if (element.style.display === "block") {
+        element.style.display = "none";
+        console.log("running");
+        more[data - 1].innerHTML = "+";
+      } else {
+        element.style.display = "block";
+        more[data - 1].innerHTML = "-";
+      }
+    } else {
+      element.style.display = "none";
+    }
+  });
+}
